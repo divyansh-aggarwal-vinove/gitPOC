d9bd220c2169b292be7b82326965a3a7f6b778da
Divyansh <61134532+divyansh-aggarwal-vinove@users.noreply.github.com>
2021-01-18 10:26:40 +0530

    new branch

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ const date = new Date();
 
 console.log("This is an assessment of Git POC\n made by Divyansh Aggarwal");
 console.log(date);
+
+console.log("This is the side branch that has been branched from main");
